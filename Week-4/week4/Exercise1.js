b108b01d2851cadf30f4091c3b4b4ac56d20e664
KavyaVatsavai <kavyavatsavai24@gmail.com>
2023-04-22 00:34:45 +0530

    4.1  excersise

--- Week-4/week4/Exercise1.js
+++ Week-4/week4/Exercise1.js
@@ -1,38 +1,50 @@
-// added function for getting random number
-function getNumber(max){
-   return  Math.floor(Math.random() * max);
-}
 
-//Custom function is created using promise function
-const asynchronousEmulator = num => {
-    return new Promise((resolve, reject) => {
-        const rand=getNumber(num);
-        let result=rand % 5;
-      setTimeout(() => {
-      
-         result === 0 ? reject(rand) : resolve(rand);
-       }, 100);
-    });
-   }
-  
-   asynchronousEmulator(100)
-  
-   .then((value) =>{
-    console.log(value+" is not divisible by 5");
-  
-  })
- 
-  .catch((err)=>{
-    console.log(err +" is divisble by 5 so caught the error");
-  
-  })
-  
-  .finally(function(){
-    console.log(asynchronousEmulator);
-  });
-   
-//output :
-//63 is not divisible by 5
-//[Function: asynchronousEmulator]
-//15 is divisble by 5 so caught the error
-//[Function: asynchronousEmulator]
+
+function getNumber(resolve, reject) {
+  let randomNumber = Math.floor(Math.random() * 100);
+  console.log("Random number is " + randomNumber + " ,So");
+  if (randomNumber % 5 == 0) {
+      resolve("Promise is resolved");
+  } else {
+      reject("Promise is rejected");
+  }
+}
+class myPromise {
+  constructor(handler) {
+      this.status = "pending";
+      this.value = null;
+
+      const resolve = value => {
+          if (this.status === "pending") {
+              this.status = "fulfilled";
+              this.value = value;
+          }
+      };
+      const reject = value => {
+          if (this.status === "pending") {
+              this.status = "rejected";
+              this.value = value;
+          }
+      };
+
+      try {
+          handler(resolve, reject);
+      } catch (err) {
+          reject(err);
+      }
+  }
+
+  then(onFulfilled, onRejected) {
+      if (this.status === "fulfilled") {
+          onFulfilled(this.value);
+      } else if (this.status === "rejected") {
+          onRejected(this.value);
+      }
+  }
+}
+const p = new myPromise(getNumber);
+p.then((res) => {
+  console.log(res);
+}, (err) => {
+  console.log(err);
+});
